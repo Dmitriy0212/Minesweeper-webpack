b3a7e2d0b8a479631e4107623f005eaeb56374f5
Dmitriy0212 <57304379+Dmitriy0212@users.noreply.github.com>
2023-05-16 21:48:27 +0300

    fix:fixed minor bugs 8

--- src/select-save.js
+++ src/select-save.js
@@ -2,20 +2,30 @@ import { timerId } from "./index";
 import { timer } from "./index";
 import { numberClicks } from "./index";
 import { mas } from "./index";
-import { masTo } from "./local-storage-save";
 import { selectSave } from "./index";
 import startFromSave from "./start-game-from-save";
 export default function toDoList() {
+    let objJson2 = JSON.stringify(localStorage);
+    let objJson3 = JSON.parse(objJson2);
+    let arr2 = [];
+    for (let key in objJson3) {
+        if (String(key).includes('Save ') !== true) {
+            continue
+        }
+        else if (String(key).includes('Save ') == true) {
+            arr2.push(JSON.parse(objJson3[key]))
+        }
+    }
     debugger
     clearInterval(timerId);
-    for (let value1 in masTo) {
-        if (this.value == masTo[value1].save) {
-            timer.textContent = masTo[value1].time
-            numberClicks.textContent = masTo[value1].clicks
+    for (let value1 in arr2) {
+        if (this.value == arr2[value1].save) {
+            timer.textContent = arr2[value1].time
+            numberClicks.textContent = arr2[value1].clicks
             for (let index in mas) {
-                mas.splice(index, 1, masTo[value1].masBomb[index])
+                mas.splice(index, 1, arr2[value1].masBomb[index])
             }
-            startFromSave(masTo[value1].masValue, masTo[value1].masStyle, masTo[value1].colorValue)
+            startFromSave(arr2[value1].masValue, arr2[value1].masStyle, arr2[value1].colorValue)
         }
     }
     selectSave.value = ''
